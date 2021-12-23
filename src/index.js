c4a3f383da063968523e5de1d9ae4bcb21435e01
Jay <originxh@gmail.com>
2021-12-23 17:07:25 +0900

    basename

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import {BrowserRouter} from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-   <BrowserRouter basename="/">
+   <BrowserRouter basename="clean-chat">
     <App />
     </BrowserRouter>
   </React.StrictMode>,
