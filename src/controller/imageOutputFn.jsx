fbecc4e5b4492164c92b9c46976951a24d4dcad4
Jay <originxh@gmail.com>
2022-03-01 16:52:39 +0900

    readme

--- src/controller/imageOutputFn.jsx
+++ src/controller/imageOutputFn.jsx
@@ -1,12 +1,12 @@
-import axios from 'axios';
+import axios from "axios";
 
-const imageOutputFn = function(url) {
-    if (url[0] === '/') {
-        url[url.length-1] !== 'g' ? 
-        url = axios.defaults.baseURL + url + 'g' : 
-        url = axios.defaults.baseURL + url;
+const imageOutputFn = function (url) {
+    if (url[0] === "/") {
+        url[url.length - 1] !== "g"
+            ? (url = axios.defaults.baseURL + url + "g")
+            : (url = axios.defaults.baseURL + url);
     }
     return url;
-}
+};
 
-export default imageOutputFn
+export default imageOutputFn;
